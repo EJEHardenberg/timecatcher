3a1f529bc1c2e3f5f39f539a140248449ad1cd64
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-09-08 01:29:29 -0400

    Added completion script
    Added that auto completes tcatch commands if sourced. Note that tcatch
    has to be in your path, so either throw it in the /usr/bin directory and
    the completion script into your bash completion directory or simply
    source it from bash_profile or something.
    
    It doesn't do as many fancy things as it probably could. We could
    detect the current task and prompt the user to switch from it or
    whatnot. It'd be an easy:
    
    if [[ -f ~/.tc/current ]] ; then
    fi
    
    And throw stuff in. But I'm not sure how to get compgen to return
    strings besides the arguments/things it's auto completeing. Yet.

---
+++ completion.sh
@@ -0,0 +1,110 @@
+_tcBase() 
+{
+    local cur prev opts base flags
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+    #
+    #  The basic options we'll complete.
+    #
+    opts="start add-info finish view --help"
+    flags="--help -h"
+
+    if [[ ${prev} == "start" ]] ; then 
+        _tcStart
+        return 0
+    fi
+
+    if [[ ${prev} == "finish" ]] ; then 
+        _tcFinish
+        return 0
+    fi
+
+    if [[ ${prev} == "view" ]] ; then 
+        _tcView
+        return 0
+    fi
+
+    if [[ ${cur} == -* ]] ; then
+        COMPREPLY=( $(compgen -W "${flags}" -- ${cur}) )
+        return 0
+    fi
+
+    if [[ ${prev} == "tcatch" ]] ; then 
+        COMPREPLY=($(compgen -W "${opts}" -- ${cur}))  
+    fi
+    return 0
+}
+_tcStart()
+{
+    local cur prev opts base
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+    opts="-s --switch -h --help"
+
+    if [[ ${cur} == -* ]] ; then
+        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+        return 0
+    fi
+
+    if [[ ${cur} == * ]] ; then
+        local tasknames=$(for x in `cat ~/.tc/indexes/*.index | cut -d ' ' -f 2- | uniq | grep -v  [[:space:]]*8 | rev | cut -d ' ' -f 2- | rev`; do echo ${x} ; done )
+        COMPREPLY=( $(compgen -W "${tasknames}" -- ${cur}) )
+        return 0
+    fi
+
+
+    COMPREPLY=($(compgen -W "${opts}" -- ${cur}))  
+    return 0
+}
+_tcFinish()
+{
+    local cur prev opts base
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+    opts="-h --help"
+
+    if [[ ${cur} == -* ]] ; then
+        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+        return 0
+    fi
+
+    local tasknames=$(for x in `cat ~/.tc/indexes/*.index | cut -d ' ' -f 2- | uniq | grep -v [[:space:]]*32 | rev | cut -d ' ' -f 2- | rev`; do echo ${x} ; done )
+    COMPREPLY=( $(compgen -W "${tasknames}" -- ${cur}) )
+    return 0
+    
+    
+}
+_tcView()
+{
+    local cur prev opts base
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+    opts="-v --verbose -h --help -a --all"
+
+    if [[ ${cur} == -* ]] ; then
+        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+        return 0
+    fi
+
+    if [[ ${cur} == * ]] ; then
+        local tasknames=$(for x in `cat ~/.tc/indexes/*.index | cut -d ' ' -f 2- | uniq | rev | cut -d ' ' -f 2- | rev`; do echo ${x} ; done )
+        COMPREPLY=( $(compgen -W "${tasknames}" -- ${cur}) )
+        return 0
+    fi
+
+
+    COMPREPLY=($(compgen -W "${opts}" -- ${cur}))  
+    return 0
+}
+complete -F _tcBase tcatch
+
+
+PATH=$PATH:~/Programming/C/timecatcher/
